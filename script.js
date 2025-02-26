068f7361006c0ab9b88330a75dee9380594b3c67
CarlosMercedes <merc0249@student.acitech.org>
2025-02-26 09:11:14 -0500

    Added to filesystem

---
+++ script.js
@@ -0,0 +1,11 @@
+const button1 = document.getElementById("button1")
+
+function yell_at_user() {
+    alert("stop pressing my button");
+}
+
+button1.addEventListener('click', yell_at_user);
+
+	console.log('Clicked!');
+
+
