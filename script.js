b00fe7d28f42d3676e3494305b1fd62e6b6c1f8c
CarlosMercedes <merc0249@student.acitech.org>
2025-02-27 21:54:35 -0500

    Made first event Made CSS styling with button and main text Made a mouse up/down animation

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const button1 = document.getElementById("button1")
+const text = document.getElementById("text")
 
-function yell_at_user() {
-    alert("stop pressing my button");
+//Animation for when you click and hold, and for when you let go
+function clickhold() {
+    button1.style.animation="clickhold .1s 1 linear forwards";
+}
+function clickleave() {
+    button1.style.animation="clickleave .5s 1 linear reverse";
+}
+button1.addEventListener("mousedown", clickhold);
+button1.addEventListener("mouseup", clickleave);
+
+function changeText1() {
+    text.textContent = "do not press this again"
 }
 
-button1.addEventListener('click', yell_at_user);
-
-	console.log('Clicked!');
-
+button1.addEventListener("click", changeText1);
 
