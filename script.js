9ebdbf9ba159f1e5654a7af4802f4879dfba76cf
CarlosMercedes <merc0249@student.acitech.org>
2025-03-03 09:14:53 -0500

    Finished green button Function call only once

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Attributes
+const buttons = document.getElementById("buttons")
 const button1 = document.getElementById("button1")
+const button1dec = document.getElementById("button1Decoration")
 const text = document.getElementById("text")
-const circle = document.getElementById("circle")
+const button2 = document.getElementById("button2")
+const button2dec = document.getElementById("button2Decoration")
 let num = document.getElementById("num")
 var count = 0
 var loseClicks = 5
@@ -10,37 +13,70 @@ window.onload = function showTimesClicked() {
     num.textContent = "Clicks: " + count
 }
 
-//Animation for when you click and hold, and for when you let go
-function clickhold() {
-    button1.style.animation="clickhold .1s 1 linear forwards";
-}
-function clickleave() {
-    button1.style.animation="clickleave .5s 1 linear forwards";
-}
-button1.addEventListener("mousedown", clickhold);
-button1.addEventListener("mouseup", clickleave);
-
 //Determine amount of clicks done and execute events based off number of clicks
+var callOnce1 = true
+var callOnce2 = true
+var callOnce10 = true
+var callOnce15 = true
+var callOnce20 = true
+
 button1.addEventListener("click", function() {
     count++;
     num.textContent = "Clicks: " + count
 
-if (count == 1) {
+if (count == 1 && callOnce1) {
     text.textContent = "do not press this again"
+    callOnce1 = false
 }
 
-if (count == 2) {
+if (count == 2 && callOnce2) {
     text.textContent = "stop"
+    callOnce2 = false
 }
 
-if (count == 10) {
+
+if (count == 10 && callOnce10) {
     text.textContent = "press the green button to win";
-    circle.style.animation="circle .5s 1 linear forwards";
+    button2.style.animation = "button2 .5s 1 linear forwards";
+    button2dec.style.animation = "button2 .5s 1 linear forwards";
+    button1.style.left = "60%"
+    button1dec.style.left = "59.45%"
+    callOnce10 = false
     }
 
-    circle.addEventListener("click", function() {
-        count = loseClicks;
+    button2.addEventListener("click", function() {
+        count = 0;
         num.textContent = "Clicks: " + count
-    });
+        text.textContent = "you just lost all your clicks"
+        button1.style.left = "44.85%"
+        button1dec.style.left = "44.3%"
+        button2.style.animation = "button2leave .5s 1 linear forwards";
+        button2dec.style.animation = "button2leave .5s 1 linear forwards";
+    }); 
+
+    if (count == 15 && callOnce15 && (button1.style.left = "44.85%")) {
+        num.textContent = "Clicks: " + count
+        text.textContent = "youre boring"
+        button1.style.left = "44.85%"
+        button1dec.style.left = "44.3%"
+        button2.style.animation = "button2leave .5s 1 linear forwards";
+        button2dec.style.animation = "button2leave .5s 1 linear forwards";
+    }
+
+    if (count == 20 && callOnce20) {
+        text.textContent = "you cant catch me"
+        button1.style.left = "76.85%"
+        button1dec.style.left = "76.3%"
+        button1.style.top = "70%"
+        button1dec.style.top = "70.5%"
+    }
+
+    if (count == 21 && callOnce20) {
+        text.textContent = "you cant catch me"
+        button1.style.left = "16.85%"
+        button1dec.style.left = "16.3%"
+        button1.style.top = "50%"
+        button1dec.style.top = "50.5%"
+    }
 });
 
